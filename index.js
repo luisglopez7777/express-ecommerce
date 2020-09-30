cee70c85c016636c9bf1691491034d31f9c5ef2a
Luis <luisglopez7777@gmail.com>
2020-09-29 19:48:11 -0500

    Modulo dos completado

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const productsRouter = require('./routes/products')
+
+app.use("/static", express.static(path.join(__dirname, 'public')))
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+app.use('/products', productsRouter)
+
+
+
+const server = app.listen(8000, () => {
+    console.log(`Listening on port http://localhost:${server.address().port}`)
+})
